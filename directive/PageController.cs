5746e9898ee0112786add799ffb04a71686a623c
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-12-15 18:32:57 +0200

    Add files via upload

---
+++ directive/PageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1112_.Controllers
+{
+    public class PageController : Controller
+    {
+        // GET: Page
+        public ActionResult Index()
+        {
+            return new FilePathResult("~/Views/Page/index.html", "text/html");
+        }
+
+        public ActionResult dir()
+        {
+            return new FilePathResult("~/Views/Page/dir.html", "text/html");
+        }
+    }
+}
---
+++ directive/parentCtrl.js
@@ -0,0 +1,35 @@
+﻿
+//let value1 
+
+module.controller("parentCtrl", ParentCtrl)
+
+// DI dependency injection - IOC
+function ParentCtrl($scope) {
+    $scope.x = 10
+    $scope.clickme = () => {
+        alert('click!')
+    }
+}
+
+// seperate file - my.directive.js
+module.directive('directiveName', function () {
+    return {
+        restrict: 'AE',
+        scope: {
+            title: '@',
+            y: '=',
+            clickFunc: '&'
+        },
+        templateUrl: 'Page/dir',
+        link: function (scope) {
+            scope.clickit = () => {
+                scope.clickFunc()()
+            }
+        }
+    }
+});
+/*
+ <div class="title"><h2>{{title}}</h2></div>
+ <div class="title"><h2>TWO WAY : <input type="number" ng-model="y" /></h2></div>
+ <button ng-click="clickit()">CLICK</button>
+*/
